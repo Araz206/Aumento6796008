e4dcd201f2cc0b31b40e6a80c10aa963ea2432c1
MINEDUCYT <MINEDUCYT@S06-15>
2024-08-19 12:31:29 -0600

    Agregar archivos de proyecto.

---
+++ Aumento6796008/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Aumento6796008
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Aumento6796008/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Aumento6796008
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
